505542d83923191cd5f842bc49852d33467a7e7e
Nitesh6206 <niteshsingh6206@gmail.com>
2025-07-12 17:08:52 +0530

    apply local changes

--- analytics-service/src/main/java/com/nitesh/analytics_service/Repository/AnalyticsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nitesh.analytics_service.Repository;
-
-import com.nitesh.analytics_service.Model.AnalyticsReport;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnalyticsRepository extends MongoRepository<AnalyticsReport, String> {
-}
--- incident-service/src/main/java/com/nitesh/incident_service/Repository/IncidentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nitesh.incident_service.Repository;
-
-import com.nitesh.incident_service.Model.Incident;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncidentRepository extends JpaRepository<Incident, Long> {
-}
